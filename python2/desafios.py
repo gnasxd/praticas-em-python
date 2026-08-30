a9415255373b092d3e18374e06824903820a6dee
Guilherme Augusto <guibiston@gmail.com>
2026-08-30 16:52:51 -0300

    modificações

--- python2/desafios.py
+++ python2/desafios.py
@@ -1,5 +1,8 @@
-n = [1,2,0,0]
+n = [1,2,3,4]
+cont = 0
+res = 0
 
-res = (n[0]*n[1])**2+(n[2]*n[3])**2
-
-print(f"O valor da conta é {res}")
+while cont<=10:
+ res = (n[0]*n[1])**2+(n[2]*n[3])**2
+ print(f"O valor da conta é {res}")
+cont = cont + 1
